c2d927e8f2f528d5dfc0f2ee130f446ed8cfd1f0
Charlie Wang <charlie-yc.wang@aia.com>
2020-04-08 14:45:16 +0800

    增加drawer、更新System配置

--- App/Config/SystemConfig.js
+++ App/Config/SystemConfig.js
@@ -6,4 +6,32 @@
  * creat by Charlie on 2020-03-24
 */
 
+import {
+    StyleSheet,
+    Platform,
+    Dimensions,
+    PixelRatio,
+    StatusBar,
+} from 'react-native';
+
+import * as UIConfig from './UIConfig';
+
 export const interceptTime = 500;     //time for intercept click the button repeatedly 
+
+export function theDeviceIsFullScreenMobilePhone() {
+    let fullScreenMobile = false;
+    if (Platform.OS === 'ios'){
+        if(UIConfig.SCREEN_WIDTH == 375 && UIConfig.SCREEN_HEIGHT == 812){
+            fullScreenMobile = true;
+        }else if (UIConfig.SCREEN_WIDTH == 414 && UIConfig.SCREEN_HEIGHT == 896){
+            fullScreenMobile = true;
+        }
+    }else if(Platform.OS === 'android'){
+
+    }
+    if(fullScreenMobile){
+        console.log('获取片面屏幕',UIConfig.SCREEN_WIDTH,UIConfig.SCREEN_HEIGHT);
+    }
+
+    return fullScreenMobile;
+}
