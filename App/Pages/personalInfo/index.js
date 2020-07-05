eb95b35938fd8eec89491ac5d4e14fb7e374a6a0
Wangzihao5325 <416990273@qq.com>
2020-07-05 21:21:24 +0800

    1.更换手机号

--- App/Pages/personalInfo/index.js
+++ App/Pages/personalInfo/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {
     View,
+    TouchableHighlight,
     Text,
     StyleSheet,
 } from 'react-native';
@@ -8,6 +9,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { themeColor } from '../../Config/UIConfig';
 
 import CustomInput from '../../Components/Component/CustomInput';
+import CustomButton from '../../Components/Component/CustomButton';
 
 export default class PersonalInfo extends Component {
     state = {
@@ -28,15 +30,27 @@ export default class PersonalInfo extends Component {
                         value={phoneNum}
                         onChangeText={this.phoneNumChange}
                     />
-                    <View style={{ marginTop: 20, height: 90, width: 350, alignSelf: 'center' }}>
+                    <View style={{ marginTop: 20, height: 45, width: 350, alignSelf: 'center', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
                         <CustomInput
                             style={{ width: 210 }}
-                            iconName='mail'
+                            iconName='mobile-phone'
                             placeholder='请输入验证码'
                             value={verificationCode}
                             onChangeText={this.verificationCodeChange}
                         />
+                        <CustomButton
+                            title='获取验证码'
+                            buttonStyle={styles.verificationCodeBtn}
+                            titleStyle={{ color: '#f2cc2e' }}
+                            clickEvent={this.getVerificationCode}
+                        />
                     </View>
+                    <CustomButton
+                        title='确定'
+                        buttonStyle={styles.confirmButton}
+                        titleStyle={{ color: '#000' }}
+                        clickEvent={this.confirm}
+                    />
                 </View>
             </SafeAreaView>
         );
@@ -53,4 +67,36 @@ export default class PersonalInfo extends Component {
             verificationCode: value
         });
     }
+
+    getVerificationCode = () => {
+        console.log('获取验证码');
+    }
+
+    confirm = () => {
+        console.log('确认');
+    }
 }
+
+const styles = StyleSheet.create({
+    verificationCodeBtn: {
+        display: 'flex',
+        height: 43,
+        width: 130,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 43,
+        borderColor: '#f2cc2e',
+        borderWidth: 1
+    },
+    confirmButton: {
+        display: 'flex',
+        height: 45,
+        width: 300,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#f2cc2e',
+        borderRadius: 45,
+        alignSelf: 'center',
+        marginTop: 60
+    }
+});
