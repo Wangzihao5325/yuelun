7ab454cd098b1dea3836a665bd3b309d1979f3d7
Wangzihao5325 <416990273@qq.com>
2020-07-13 11:30:20 +0800

    1.我的页面优化

--- App/store/index.js
+++ App/store/index.js
@@ -2,10 +2,12 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import ReduxThunk from 'redux-thunk';
 import testReducer from './reducers/testReducer';
 import appReducer from './reducers/appReducer';
+import userReducer from './reducers/userReducer';
 
 const rootReducer = combineReducers({
     test: testReducer,
-    app: appReducer
+    app: appReducer,
+    user: userReducer
 });
 
 const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
