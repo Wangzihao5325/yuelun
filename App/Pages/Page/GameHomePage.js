3bf00ac0ba77f7a45a1ef418fc84ce34fadafccb
yichen wang <yichenwang@yichens-MacBook-Pro.local>
2020-07-19 22:27:55 +0800

    更新首页UI渲染并对接数据

--- App/Pages/Page/GameHomePage.js
+++ App/Pages/Page/GameHomePage.js
@@ -27,6 +27,7 @@ import GameNormalItem from '../../Components/Component/Game/GameNormalItem';
 
 import PageName from '../../Config/PageName';
 import * as navigator from '../../Router/NavigationService';
+import * as mock_home from '../../Mock/home';
 
 let testData = [
     { imageUrl: 'http://b.hiphotos.baidu.com/zhidao/pic/item/c75c10385343fbf233e9732cb27eca8064388ffc.jpg' },
@@ -39,11 +40,50 @@ export default class acceleratorPage extends Component {
         this.state = {
             selectStatus:1,
             isRefreshingStatus:false,
+
+            allGame:{},
+            overseasGames:{},
+            upcomingGames:{},
+
+            all_game_collection:[],
+            all_game_hot:[],
+            all_game_new:[],
+
+            overseas_games:[],
+            upcoming_games:[]
         }
     }
 
     componentDidMount(){
-       
+        //国服数据解析
+        console.log('mock_homemock_home',mock_home['default']['data']['gameList'][1]['game_list']['精选']);
+        let all_game_collection = mock_home['default']['data']['gameList'][1]['game_list']['精选'];
+        let all_game_hot = mock_home['default']['data']['gameList'][1]['game_list']['热门'];
+        let all_game_new = mock_home['default']['data']['gameList'][1]['game_list']['最新'];
+
+        //外服数据解析
+        let overseas_game_colloection = mock_home['default']['data']['gameList'][2]['game_list']['精选'];
+        let overseas_game_hot = mock_home['default']['data']['gameList'][2]['game_list']['热门'];
+        let overseas_game_new = mock_home['default']['data']['gameList'][2]['game_list']['最新'];
+
+        if(!overseas_game_colloection) overseas_game_colloection = [];
+        if(!overseas_game_hot) overseas_game_hot = [];
+        if(!overseas_game_new) overseas_game_new = [];
+        let overseas_games = [];
+        overseas_games.push(...overseas_game_colloection);
+        overseas_games.push(...overseas_game_hot);
+        overseas_games.push(...overseas_game_new);
+
+        //解析即将上线
+        let upcoming_games = mock_home['default']['data']['gameList'][0]['game_list'];
+
+       this.setState({
+        all_game_collection:all_game_collection,
+        all_game_hot:all_game_hot,
+        all_game_new:all_game_new,
+        overseas_games:overseas_games,
+        upcoming_games:upcoming_games
+       });
         
     }
 
@@ -102,18 +142,24 @@ export default class acceleratorPage extends Component {
     renderTheAllGamePage = () =>{
         return(
             <View>
-                {this.renderTheGameSection(1,'精选游戏',require('../../resource/Image/GameHomePage/diamond.png'))}
-                {this.renderTheGameSection(2,'热门游戏',require('../../resource/Image/GameHomePage/new.png'))}
-                {this.renderTheGameSection(3,'最新游戏',require('../../resource/Image/GameHomePage/hot.png'))}  
+                {this.renderTheGameSection(1,'精选游戏',require('../../resource/Image/GameHomePage/diamond.png'),this.state.all_game_collection)}
+                {this.renderTheGameUnitSection(2,'热门游戏',require('../../resource/Image/GameHomePage/new.png'),this.state.all_game_hot)}
+                {this.renderTheGameUnitSection(3,'最新游戏',require('../../resource/Image/GameHomePage/hot.png'),this.state.all_game_new)}  
             </View>
         );
     }
 
     //渲染常规四列一纵的游戏页面
     renderTheNormalGamePage = () =>{
+        let dataArray = [];
+        if(this.state.selectStatus == 2){
+            dataArray = this.state.overseas_games;
+        }else if (this.state.selectStatus == 3){
+            dataArray = this.state.upcoming_games;
+        }
         return(
-            <View>
-                {this.renderTheNormalGameItems()}
+            <View style={styles.normalRootViewStyle}>
+                {this.renderTheNormalGameItems(dataArray)}
             </View>
         );
     }
@@ -126,62 +172,123 @@ export default class acceleratorPage extends Component {
                      iconSource = {iconSource} 
                      title={title}
                      clickFunction = {()=>{
-
+                         console.log('查看',title,'的更多');
                      }}/>
                      
-                <ScrollView      
+                <ScrollView   
+                     contentContainerStyle={{flexWrap:"nowrap"}}
+                     height = {dataArray.length <= 4 ? 100 : 200}   
                      horizontal = {true}
                      keyboardDismissMode = 'on-drag'
                      >
-                         {this.renderTheGanmeItems(type)}
+                         {this.renderTheGanmeItems(type,dataArray)}
                 </ScrollView>
             </View>
         );
     }
 
+    renderTheGameUnitSection = (type=0,title = '',iconSource = require('../../resource/Image/GameHomePage/diamond.png'), dataArray = []) =>{
+        return(
+            <View style={{marginLeft:0,marginTop:10,width:SCREEN_WIDTH,}}>
+                <GameTitleItem
+                     iconSource = {iconSource} 
+                     title={title}
+                     clickFunction = {()=>{
+                         console.log('查看',title,'的更多');
+                     }}/>
+                     
+                <ScrollView   
+                     contentContainerStyle={{flexWrap:"nowrap"}}
+                     height = {dataArray.length > 2 ? 270 : 135}   
+                     horizontal = {true}
+                     keyboardDismissMode = 'on-drag'
+                     >
+                         {this.renderTheGanmeItems(type,dataArray)}
+                </ScrollView>
+            </View>
+        );
+    }
 
-
-    renderTheGanmeItems = (type=0) =>{
-        let test;
+    /**
+     * 渲染游戏列表展示区域
+     * 
+    */
+    renderTheGanmeItems = (type=0,dataArray=[]) =>{
+        let gameItems;
         if(type == 1){
-            test = [this.renderTheGameNormalUnitItem(),this.renderTheGameNormalUnitItem(),this.renderTheGameNormalUnitItem(),this.renderTheGameNormalUnitItem(),this.renderTheGameNormalUnitItem(),this.renderTheGameNormalUnitItem()];
+            gameItems = [this.renderTheGameNormalUnitItem(dataArray)];
         }else{
-            test = [this.renderTheGameUnitItem(),this.renderTheGameUnitItem(),this.renderTheGameUnitItem(),this.renderTheGameUnitItem()];
+            gameItems = [this.renderTheGameUnitItem(dataArray)];
         }
-        return test;
+        return gameItems;
     }
 
-    renderTheNormalGameItems = () =>{
-        let test = [this.renderTheNormalItemCell(),this.renderTheNormalItemCell(),this.renderTheNormalItemCell(),this.renderTheNormalItemCell(),this.renderTheNormalItemCell(),this.renderTheNormalItemCell(),this.renderTheNormalItemCell(),this.renderTheNormalItemCell(),this.renderTheNormalItemCell()];
-        return test;
+    renderTheNormalGameItems = (dataArray = []) =>{
+        let unitItems = [];
+
+        dataArray.map(renderNormalCell = (item,index) => {
+            let unitItem = this.renderTheNormalGameItem(item,index);
+            unitItems.push(unitItem);
+         });
+
+        return unitItems;
     }
 
-
-    renderTheGameUnitItem = () =>{
+    renderTheNormalGameItem = (item, index) =>{
+        let unitWidth = SCREEN_WIDTH/4;
         return(
-            <View style={styles.scrollViewStyle}>
-                <GameUnitItem nameText={'steam移动版\n(亚服)'}/>
-                <GameUnitItem nameText={'steam移动版\n(亚服)'}/>
+            <View style={[styles.normalItemRootCell,{width:unitWidth}]}>
+                 <GameNormalItem 
+                     index = {index}
+                     source = {{uri:item.icon}}
+                     title={item['name']} 
+                     showFavoratorIcon={true} 
+                     favorator={false}/>
             </View>
         );
     }
 
-    renderTheGameNormalUnitItem = () =>{
+    /**
+     * 渲染最新、热门游戏的UI布局
+     * 
+    */
+    renderTheGameUnitItem = (dataArray = []) =>{
         return(
-            <View style={[styles.scrollViewStyle,{height:200}]}>
-                <GameNormalItem title={'和平精英'}/>
-                <GameNormalItem title={'PUBG MOBILE\n(亚服)'}/>
+            <View style={[styles.scrollViewStyleOne,{height: dataArray.length > 2 ? 270 : 135}]}>
+                {
+                    dataArray.map(function (item,inedx) {
+                        return(
+                             <View style={{marginLeft:10}}>
+                                 <GameUnitItem 
+                                 key = {inedx} 
+                                 nameText={item['name']}
+                                 source = {{uri:item.icon}}/>
+                                 </View>);
+                    })
+                }
             </View>
         );
     }
 
-    renderTheNormalItemCell = (dataArrya=[]) =>{
+    /**
+     * 渲染常规的游戏icon布局
+     * 
+    */
+    renderTheGameNormalUnitItem = (dataArray = []) =>{
         return(
-            <View style={styles.normalItemRootCell}>
-                <GameNormalItem title={'和平精英'} showFavoratorIcon={true} favorator={false}/>
-                <GameNormalItem title={'和平精英'} showFavoratorIcon={true} favorator={false}/>
-                <GameNormalItem title={'和平精英'} showFavoratorIcon={true} favorator={true}/>
-                <GameNormalItem title={'和平精英'} showFavoratorIcon={true} favorator={true}/>
+            <View style={[styles.scrollViewStyleOne,{height: dataArray.length > 4 ? 200 : 100}]}>
+                {
+                    dataArray.map(function (item,inedx) {
+                        return(
+                             <View style={{marginLeft:5}}>
+                                 <GameNormalItem 
+                                     key = {inedx} 
+                                     title={item['name']}
+                                     source = {{uri:item.icon}}
+                                 />
+                            </View>);
+                    })
+                }
             </View>
         );
     }
@@ -192,7 +299,7 @@ export default class acceleratorPage extends Component {
     }
 
     clickTheSearchItemButton = () =>{
-        navigator.jump(this, PageName.NORMAL_PAGE_SETTING);
+        navigator.jump(this, PageName.NORMAL_PAGE_SEARCH);
     }
 }
 
@@ -212,12 +319,24 @@ const styles = StyleSheet.create({
         justifyContent:'space-around',
         marginLeft:10.5
     },
+    scrollViewStyleOne:{
+        flexDirection:'column',
+        justifyContent:'flex-start',
+        flexWrap:'wrap',
+        justifyContent:'space-between',
+    },
     normalItemRootCell:{
-        width:SCREEN_WIDTH,
         height:100,
-        flexDirection:'row',
-        alignItems:'center',
-        justifyContent:'space-around'
+        marginTop:10,
+        alignItems:'center'
+    },
+    normalRootViewStyle:{
+        marginLeft:0,
+        marginTop:0,
+        width:SCREEN_WIDTH,
+        flex:1,
+        flexWrap:'wrap',
+        flexDirection:'row'
     }
    
 });
